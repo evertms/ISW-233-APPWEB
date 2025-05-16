f7d0f64aab69beae539f652b02d83fcf7bf7a2a4
Evert Moreno Serrate <evert23122004@gmail.com>
2025-05-16 02:47:47 -0400

    Generando los componentes cards
    Con DOM API

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 // TODO: Use the DOM API to create the card components
-const html = `<article class="card">
-<h3 class="card__title"></h3>
-<div class="card__body">
-  <div class='card__body__image'></div>
-  <section class='card__body__content'>
-  </section>
-</div>
-</article>`;
-
 // 1. Seleccione el container
+const container = document.getElementById("container");
 
-// 2. cree una funciotn createCardComponent.
+// 2. cree una función createCardComponent
 /**
  * @param {string} title
  * @param {string} body
  *
  * @return {HTMLElement}
  */
+function createCardComponent(title, body) {
+    const template = document.getElementById("card__template");
+    const element = template.content.cloneNode(true).firstElementChild;
+    
+    const elementTitle = element.querySelector(".card__title");
+    elementTitle.textContent = title;
+    
+    const elementBody = element.querySelector(".card__body__content");
+    elementBody.textContent = body;
+    
+    return element;
+}
 
-// 3. Cree un componente
+// 3. Crear componente de ejemplo
+const card1 = createCardComponent("Fundamentals 1", "Some random text here");
 
-// 4. Agrege este nuevo componente al container
+// 4. Agregar los componentes al container
+container.appendChild(card1);
